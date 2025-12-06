f7347f108c64ebc0bbd49833309af932dc38f55f
Adam Simons <adam9812@live.com>
2025-12-05 19:30:46 -0600

    test if statement

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
 function love.load()
-   
+   testVariable = 439237424322
 end
 function love.draw()
    love.graphics.print("Hallo Welt 3", 200, 200)
+   if testVariable % 2 == 0 then
+      love.graphics.print(testVariable .. " is even", 300, 200)
+   else
+      love.graphics.print(testVariable .. " is odd", 300, 200)
+   end
 end
